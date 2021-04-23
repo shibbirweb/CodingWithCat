2bf2cdcd374d25c3534920b8941e8f782816345c
DJSeokHo <djseokho@vip.qq.com>
2021-04-23 15:31:08 +0900

    Seok Ho

---
+++ Android/Kotlin2021/CustomizeColorSwitchExample/app/src/main/java/com/swein/customizecolorswitchexample/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.swein.customizecolorswitchexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Hello, let's build a customize color switch
+ *
+ * 1. build shape when switch off
+ * - build thumbnail
+ * - build track
+ *
+ * 2. build shape when switch on
+ * - build thumbnail
+ * - build track
+ *
+ * 3. build thumbnail selector
+ * 4. build track selector
+ *
+ * 5. xml
+ * 6. let's run app
+ *
+ * thanks
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
